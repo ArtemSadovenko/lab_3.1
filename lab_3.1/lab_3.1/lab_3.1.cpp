cf97fb0c245e29b2485c898307ddadb43c4bf1d8
ArtemSadovenko <90398799+ArtemSadovenko@users.noreply.github.com>
2021-09-30 16:08:27 +0300

    Add files via upload

---
+++ lab_3.1/lab_3.1/lab_3.1.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    double x;
+    double y;
+
+    cout << "x = "; cin >> x;
+
+    if (x <= -1) {
+        y = 13.5 - (2 * x) - exp(0.4+x);
+    }
+     else if (x > -1 && x < 1) {
+        y = 13.5 - (2 * x) - 1 - pow((sin(x)), 2);
+    }
+        else if (x >= 1) {
+        y = 13.5 - (2 * x) - ((cos(x))/(1 + pow((sin(x)), 2)));
+    }
+
+    cout << y << endl;
+
+    return 0;
+}
